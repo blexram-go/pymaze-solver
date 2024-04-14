ddfad2bc379e1b6fd487b099eda90ecc82ab291e
gobash-blex <brian_ramos_14@yahoo.com>
2024-04-13 21:43:14 -0700

    Create a cell and maze file to add further functionality to maze solver as well as satisfy SOC

--- graphics.py
+++ graphics.py
@@ -5,9 +5,12 @@ class Window:
         self.root = Tk()
         self.root.title("PyMaze Solver")
         self.canvas = Canvas(self.root, bg="white", height=height, width=width)
-        self.canvas.pack()
+        self.canvas.pack(fill=BOTH, expand=1)
         self.window_running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def draw_line(self, line, fill_color="red"):
+        line.draw(self.canvas, fill_color)
 
     def redraw(self):
         self.root.update_idletasks()
@@ -21,4 +24,17 @@ class Window:
         
     def close(self):
         self.window_running = False
-        
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color="red"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
